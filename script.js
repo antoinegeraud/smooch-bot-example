d46f6f5de3376967d7f7b591523ab9abb5fd6e6d
antoine <antoine@inchbase.com>
2016-05-02 14:04:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 const Script = require('smooch-bot').Script;
@@ -10,24 +11,27 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Bonjour! Je m\'appelle Estèphe!')
+            return bot.say('Hi! I\'m Smooch Bot!')
                 .then(() => 'askName');
         }
     },
 
     askName: {
-        prompt: (bot) => bot.say('Comment t\'appelles-tu ?'),
+        prompt: (bot) => bot.say('What\'s your name?'),
         receive: (bot, message) => {
             const name = message.text;
             return bot.setProp('name', name)
-                .then(() => bot.say(`Super! Je vais t'appeler ${name}`))
+                .then(() => bot.say(`Great! I'll call you ${name}`))
                 .then(() => 'finish');
-        },
+        }
+    },
+
     finish: {
         receive: (bot, message) => {
             return bot.getProp('name')
-                .then((name) => bot.say(`Désolé ${name}, mon créateur ne m'a pas ` +
-                        'appris à faire autre chose!'))
+                .then((name) => bot.say(`Sorry ${name}, my creator didn't ` +
+                        'teach me how to do anything else!'))
                 .then(() => 'finish');
         }
+    }
 });
